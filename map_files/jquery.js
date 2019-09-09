243409af05431e4051948a3605a940277f78e7d7
ivy0528 <51935958+ivy0528@users.noreply.github.com>
2019-09-09 16:43:04 +0800

    Add files via upload

---
+++ map_files/jquery.js
@@ -0,0 +1,17 @@
+﻿var number=8;
+
+function LMYC() {
+var lbmc;
+    for (i=1;i<=number;i++) {
+        lbmc = eval('LM' + i);
+        lbmc.style.display = 'none';
+    }
+}
+ 
+function ShowFLT(i) {
+    lbmc = eval('LM' + i);
+    if (lbmc.style.display == 'none') {
+        LMYC();
+        lbmc.style.display = '';
+    }
+}
